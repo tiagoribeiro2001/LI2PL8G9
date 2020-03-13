22ed3e0b3e9bb03bdf7cf88b11f37528193c6b58
A93203 <a93203@alunos.uminho.pt>
2020-03-13 12:07:37 +0000

    Alterações na monstrar_tabuleiro

--- TrabalhoLI2/interface.c
+++ TrabalhoLI2/interface.c
@@ -6,23 +6,23 @@ void mostrar_tabuleiro(ESTADO *e) {
     for (int i=0; i<8; i++) {
         if (i == 0) {
             for (int j = 0; j < 7; j++) {
-                if (e->tab[j][i] == VAZIO) printf(".");
-                if (e->tab[j][i] == BRANCA) printf("*");
-                if (e->tab[j][i] == PRETA) printf("#");
+                if (e->tab[i][j] == VAZIO) printf(".");
+                if (e->tab[i][j] == BRANCA) printf("*");
+                if (e->tab[i][j] == PRETA) printf("#");
             }
-            if (e->tab[7][0] == BRANCA)  printf("*");
+            if (e->tab[0][7] == BRANCA)  printf("*");
             else printf("2");
         }
         else if (i == 7) {
             for (int p = 0; p < 8; p++) {
                 if (p == 0) {
-                    if (e->tab[0][7] == BRANCA) printf("*");
+                    if (e->tab[7][0] == BRANCA) printf("*");
                     else printf("1");
                 }
                 else {
-                    if (e->tab[p][i] == VAZIO) printf(".");
-                    if (e->tab[p][i] == BRANCA) printf("*");
-                    if (e->tab[p][i] == PRETA) printf("#");
+                    if (e->tab[i][p] == VAZIO) printf(".");
+                    if (e->tab[i][p] == BRANCA) printf("*");
+                    if (e->tab[i][p] == PRETA) printf("#");
                 }
             }
         }
