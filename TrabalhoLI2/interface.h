b23fb67f9e7af8ef92584a1480af1baf8790305f
A93203 <a93203@alunos.uminho.pt>
2020-03-21 17:48:32 +0000

    Adicionados comentários no doxygen

--- TrabalhoLI2/interface.h
+++ TrabalhoLI2/interface.h
@@ -1,16 +1,34 @@
+/**
+@file interface.h
+Funções utilizadas na interface do jogo
+*/
 #ifndef TRABALHOLI2_INTERFACE_H
 #define TRABALHOLI2_INTERFACE_H
 
 #include "dados.h"
 
+/**
+\brief Imprime o tabuleiro
+@param e Apontador para o estado
+*/
 void mostrar_tabuleiro(ESTADO *e);
 
+/**
+\brief Imprime o prompt
+@param e Apontador para o estado
+*/
 void prompt (ESTADO *e);
 
+/**
+\brief Lê o que o utilizador escreve no programa
+@param e Apontador para o estado
+*/
 int interpretador(ESTADO *e);
 
+/**
+\brief Imprime a mensagem que congratula o jogador vencedor
+@param e Apontador para o estado
+*/
 void congratula_jogador (ESTADO *e);
-
-void jogada_invalida ();
 
 #endif //TRABALHOLI2_INTERFACE_H
--- TrabalhoLI2/logica.h
+++ TrabalhoLI2/logica.h
@@ -1,14 +1,40 @@
+/**
+@file logica.h
+Parte lógica do programa
+*/
 #ifndef TRABALHOLI2_LOGICA_H
 #define TRABALHOLI2_LOGICA_H
 
 #include "dados.h"
 
+/**
+\brief Altera os parâmetros no estado mediante a jogada
+@param e Apontador para o estado
+@param c A coordenada da jogada a efetuar
+@returns Devolve um se for possível jogar e zero caso não seja possível.
+*/
 int jogar(ESTADO *e, COORDENADA c);
 
+/**
+\brief Verifica se a jogada introduzida é valida
+@param e Apontador para o estado
+@param c A coordenada da jogada a efetuar
+@returns Devolve um se for possível jogar e zero caso não seja possível.
+*/
 int valida_jogada(ESTADO *e, COORDENADA c);
 
+/**
+\brief Verifica se o jogo chegou ao fim
+@param e Apontador para o estado
+@returns Devolve zero caso o jogo tenha acabado ou um em caso contrário.
+*/
 int verifica_fim(ESTADO *e);
 
+/**
+\brief Verifica qual jogador venceu o jogo
+@param e Apontador para o estado
+@returns Devolve um caso o jogador um tenha ganho ou dois caso o jogador dois tenha ganho.
+*/
 int verifica_vencedor(ESTADO *e);
 
 #endif
