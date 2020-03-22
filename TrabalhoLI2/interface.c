72ef5450deac5b484a14f62eae653852e8ac1de4
a89567 <a89567@alunos.uminho.pt>
2020-03-22 18:58:55 +0000

    Adicionados os comandos gr, ler e Q ao interpretador

--- TrabalhoLI2/interface.c
+++ TrabalhoLI2/interface.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "logica.h"
+#include <stdlib.h>
 
 #define BUF_SIZE 1024
 
@@ -23,20 +24,54 @@ void prompt (ESTADO *e){
 
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
+    char *token;
     char col[2], lin[2];
     if(fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
-    if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
-        COORDENADA coord = {*col - 'a', 7-(*lin - '1')};
-        if (valida_jogada(e,coord)){
-            jogar(e, coord);
-            putchar('\n');
+    printf("Jogador: %s\n",linha);
+    token = strtok(linha," ");
+    if (strcmp(token, "Q\n") == 0) {
+        printf("A sair do programa.\n");
+        exit(0);
+    }
+    else if (strcmp(token, "gr") == 0) {
+        // Ler nome do ficheiro
+        printf("Xico: ");
+        token = strtok(NULL, " \n");
+        if (token != NULL) {
+            printf("Gravar o jogo no seguinte ficheiro: %s\n", token);
+            FILE * ficheiro;
+            ficheiro = fopen(token,"wb");
+            fwrite(e,sizeof(ESTADO),1,ficheiro);
+            fclose(ficheiro);
+            prompt(e);
+        }
+    }
+    else if (strcmp(token, "ler") == 0) {
+        // Ler nome do ficheiro
+        printf("Xico1: ");
+        token = strtok(NULL, " \n");
+        if (token != NULL) {
+            printf("Ler o jogo no seguinte ficheiro: %s\n", token);
+            FILE * ficheiro;
+            ficheiro = fopen(token,"rb");
+            fread(e,sizeof(ESTADO),1,ficheiro);
+            fclose(ficheiro);
             mostrar_tabuleiro(e);
             prompt(e);
         }
-        else jogada_invalida();
     }
-    else jogada_invalida();
+    else {
+        if (strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
+            COORDENADA coord = {*col - 'a', 7 - (*lin - '1')};
+            if (valida_jogada(e, coord)) {
+                jogar(e, coord);
+                putchar('\n');
+                mostrar_tabuleiro(e);
+                prompt(e);
+            } else jogada_invalida();
+        } else jogada_invalida();
+    }
     return 1;
 }
 
