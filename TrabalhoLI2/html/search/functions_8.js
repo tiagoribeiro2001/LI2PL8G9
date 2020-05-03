3506529d10772fc375ba6ebefab083c6210c1c30
A93203 <a93203@alunos.uminho.pt>
2020-05-03 11:31:34 +0100

    Preparações para a entrega

---
+++ TrabalhoLI2/html/search/functions_8.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['obter_5fcasa',['obter_casa',['../dados_8h.html#a108e70c2c38330ddb772d81de8954813',1,'dados.c']]],
+  ['obter_5fjogadas_5fjog1_5fcoluna',['obter_jogadas_jog1_coluna',['../dados_8h.html#aec1daed5afd3ef6c89dd7dc79b9f8bf7',1,'dados.c']]],
+  ['obter_5fjogadas_5fjog1_5flinha',['obter_jogadas_jog1_linha',['../dados_8h.html#adcaa76ebdfef08f3d12517838b34a311',1,'dados.c']]],
+  ['obter_5fjogadas_5fjog2_5fcoluna',['obter_jogadas_jog2_coluna',['../dados_8h.html#acc2b815b1ebcf1cdf203a6ca1b22e998',1,'dados.c']]],
+  ['obter_5fjogadas_5fjog2_5flinha',['obter_jogadas_jog2_linha',['../dados_8h.html#a76d9309066da600d6e3dd1d8c75f6708',1,'dados.c']]],
+  ['obter_5fjogador_5fatual',['obter_jogador_atual',['../dados_8h.html#acc6c25533188a99cb69c12e7f2c406ef',1,'dados.c']]],
+  ['obter_5fnumero_5fcomandos',['obter_numero_comandos',['../dados_8h.html#adf753ce414d743fcaf922e4ec42a4b06',1,'dados.c']]],
+  ['obter_5fnumero_5fde_5fjogadas',['obter_numero_de_jogadas',['../dados_8h.html#abbfeab93575f20e5867482fd41a71cba',1,'dados.c']]],
+  ['obter_5fnumj_5fpos',['obter_numj_pos',['../dados_8h.html#aff00b5bf9dc40bbff1e1d313be850fe6',1,'dados.c']]],
+  ['obter_5ftotal_5fjogadas',['obter_total_jogadas',['../dados_8h.html#a8b174742d889842727b5860e3c52e754',1,'dados.c']]],
+  ['obter_5fultima_5fjogada',['obter_ultima_jogada',['../dados_8h.html#a40555aff97afc67bd1866f2785111310',1,'dados.c']]],
+  ['obter_5fultima_5fjogada_5fcoluna',['obter_ultima_jogada_coluna',['../dados_8h.html#afc54a1ee3e093de317342e4ffcd2b893',1,'dados.c']]],
+  ['obter_5fultima_5fjogada_5flinha',['obter_ultima_jogada_linha',['../dados_8h.html#a4f530e36ba84a0ebf4da8d66b9abcf3c',1,'dados.c']]]
+];
---
+++ TrabalhoLI2/html/search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['proximo',['proximo',['../structnodo.html#aaaabdcb6641ddcdac8990402242b154a',1,'nodo']]]
+];
